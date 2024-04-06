c05958cd97e4724aad84e8f2599d48a9d9b8eefe
Natalie-Dor <varnata000@gmail.com>
2024-04-06 17:49:19 +0300

    update task 3

--- js/task-3.js
+++ js/task-3.js
@@ -4,27 +4,5 @@ const nameOutput = document.querySelector("#name-output");
 nameInput.addEventListener("input", listenerInput);
 function listenerInput(event) {
   const trimmedValue = event.currentTarget.value.trim();
-  if (trimmedValue !== "") {
-    nameOutput.textContent = trimmedValue;
-  }
+  nameOutput.textContent = trimmedValue !== "" ? trimmedValue : "Anonymous";
 }
-//annotation==============================================================================================
-// Використання оригінального ВмістуТексту: Використання змінної text для збереження оригінального textContent елементу nameOutput є зайвим. В завданні зазначено, що якщо введення порожнє або містить лише пробіли, має відображатися "Анонім". Отже, немає потреби зберігати та використовувати спочатку заданий текст привітання, оскільки воно має бути встановлене напряму на "Анонім", коли це застосовується.
-//???????????????????????????????????????????????????????????????????????????????????????????????????????
-// без збереження оригінального тексту працює некоректно - якщо написати будь що в інпут, а потім витерти - то залишається перший символ в спані.
-
-// BEFORE =================================================================================================
-
-// const nameInput = document.querySelector("#name-input");
-// const nameOutput = document.querySelector("#name-output");
-// const text = nameOutput.textContent;
-
-// nameInput.addEventListener("input", listenerInput);
-// function listenerInput(event) {
-//   const trimmedValue = event.currentTarget.value.trim();
-//   if (trimmedValue !== "") {
-//     nameOutput.textContent = trimmedValue;
-//   } else {
-//     nameOutput.textContent = text;
-//   }
-// }
